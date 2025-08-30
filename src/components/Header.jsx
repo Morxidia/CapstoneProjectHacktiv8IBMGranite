3a03f40e8c264a30b9b26ade0ff84df7307a342b
Morxidia <morxidia@gmail.com>
2025-08-30 17:04:32 +0700

    last update before documentation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ function Header() {
   });
 
   return (
-    <header className="mb-8">
+    <header className="mb-2">
       <div className="flex items-center justify-between">
         <div>
-          <h1 className="text-2xl font-bold text-slate-800">All Task</h1>
+          <h1 className="text-2xl font-bold text-slate-800">Crankly Task Manager</h1>
           <p className="text-slate-600">{currentDate}</p>
         </div>
       </div>
